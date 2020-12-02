c6ffc10cea603cb12ab3ba5108ba859c36f17580
eunsunShin <unusual0218@gmail.com>
2020-12-02 10:22:32 +0900

    myCall Details 페이지 레이아웃 완성

---
+++ pages/myCall/details/components/proposed/Proposed.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classNames from "classnames/bind";
+import styles from "./proposed.scss";
+
+import Detailinfo from "../../../../../components/molecules/detailInfo/DetailInfo";
+
+const cx = classNames.bind(styles);
+
+const Proposed = () => {
+  return (
+    <div className={cx("proposed")}>
+      <Detailinfo title="제안차량 1" content="제네시스 G90"/>
+      <Detailinfo title="제안차량 2" content="01가1234"/>
+      <Detailinfo title="추가 요청사항" content="-"/>      
+    </div>
+  );
+};
+
+export default Proposed;
