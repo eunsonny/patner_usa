c56e797d8bf2f385606623d0c2edaca0c1fd7929
deokyeong1020@gmail.com <deokyeong1020@gmail.com>
2020-12-03 13:14:52 +0900

    Add : 마이콜 디테일 페이지와 관계적 연결 기능 구현       -  API 통신 추가

--- pages/myCall/cardTwo/index.js
+++ pages/myCall/cardTwo/index.js
@@ -14,33 +14,35 @@ const CardTwo = ({ menuOnOff, info, menuNum, id }) => {
         <div className={cx("container")}>
           <div className={cx("request")}>
             <span className={cx("requestType")}>공업사 요청</span>
-            <span className={cx("requestNum")}>{`요청번호 11940`}</span>
+            <span
+              className={cx("requestNum")}
+            >{`요청번호 ${info.request_id}`}</span>
           </div>
           <div className={cx("info")}>
             <div className={cx("carType")}>
               <span>고객차종</span>
-              <span>{info.car}</span>
+              <span>{info.customer_car}</span>
             </div>
             {!menuOnOff[4] && (
               <div className={cx("addInfo")}>
                 <span>차량번호</span>
-                <span>{info.number}</span>
+                <span>{info.customer_car_number}</span>
               </div>
             )}
             <div className={cx("addInfo")}>
               <span>요청일</span>
-              <span>{info.date}</span>
+              <span>{timeCacl(info.request_time)}</span>
             </div>
             {!menuOnOff[2] && (
               <div className={cx("addInfo")}>
                 <span>배차일</span>
-                <span>{info.useDate}</span>
+                <span>{timeCacl(info.dispatch_time)}</span>
               </div>
             )}
             {menuOnOff[4] && (
               <div className={cx("addInfo")}>
                 <span>반납일</span>
-                <span>{info.complete}</span>
+                <span>{timeCacl(info.return_time)}</span>
               </div>
             )}
           </div>
@@ -51,3 +53,16 @@ const CardTwo = ({ menuOnOff, info, menuNum, id }) => {
 };
 
 export default CardTwo;
+
+function timeCacl(time) {
+  if (time !== "None") {
+    const timeArr = time.split(" ");
+
+    const YMD = timeArr[0];
+    const T = timeArr[1].split(".")[0];
+
+    return `${YMD} ${T}`;
+  } else {
+    return ``;
+  }
+}
